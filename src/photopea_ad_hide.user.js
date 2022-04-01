1b48d0a7893a2023b32da3b6c4d6e68c95d2aae5
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-04-01 17:21:25 +0200

    style: remove url end slashes

--- src/photopea_ad_hide.user.js
+++ src/photopea_ad_hide.user.js
@@ -5,9 +5,9 @@
 // @description     Hides advertisements by pushing them out of view.
 // @author          Alejandro Akbal
 // @icon            https://www.google.com/s2/favicons?sz=64&domain=photopea.com
-// @homepage        https://github.com/AlejandroAkbal/Photopea-Advertisement-Hide/
+// @homepage        https://github.com/AlejandroAkbal/Photopea-Advertisement-Hide
 // @downloadURL     https://raw.githubusercontent.com/AlejandroAkbal/Photopea-Advertisement-Hide/main/src/photopea_ad_hide.user.js
-// @match           https://www.photopea.com/
+// @match           https://www.photopea.com
 // @grant           none
 // @run-at          document-start
 // ==/UserScript==
