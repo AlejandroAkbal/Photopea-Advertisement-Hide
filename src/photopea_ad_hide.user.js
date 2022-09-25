0e424dae12e39a45f78fd8f8cac424563796e9e9
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-09-25 16:09:38 +0000

    fix(build): add upload url

--- src/photopea_ad_hide.user.js
+++ src/photopea_ad_hide.user.js
@@ -7,6 +7,7 @@
 // @icon            https://www.google.com/s2/favicons?sz=64&domain=photopea.com
 // @homepage        https://github.com/AlejandroAkbal/Photopea-Advertisement-Hide
 // @downloadURL     https://raw.githubusercontent.com/AlejandroAkbal/Photopea-Advertisement-Hide/main/src/photopea_ad_hide.user.js
+// @uploadURL       https://raw.githubusercontent.com/AlejandroAkbal/Photopea-Advertisement-Hide/main/src/photopea_ad_hide.user.js
 // @match           https://www.photopea.com
 // @grant           none
 // @run-at          document-start
